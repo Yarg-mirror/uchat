fb61d2f156c9df44eb5b30d5c7e102c9c250cb27
Kane <kane@yarg.fr>
2026-07-29 22:36:15 +0200

    adding a client part

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"flag"
+	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -26,13 +28,36 @@ func main() {
 	serverName := flag.String("name", randString(32), "The server name")
 	serverAddress := flag.String("address", "0.0.0.0", "The server listen address")
 	serverPort := flag.Int("port", 35498, "The server port")
+	message := flag.String("msg", "", "The message to send")
 	flag.Parse()
-	server := server.New(*serverName, *serverPort)
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-	go server.Serve(*serverAddress)
+	if *message == "" {
+		server := server.New(*serverName, *serverPort)
 
-	<-ctx.Done()
-	server.Close()
+		ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+		defer stop()
+		go server.Serve(*serverAddress)
+
+		<-ctx.Done()
+		server.Close()
+	} else {
+
+		dst := "127.0.0.1"
+		dstIP := net.ParseIP(dst)
+		dstAddress := net.UDPAddr{IP: dstIP, Port: 35498}
+		connection, err := net.DialUDP("udp", nil, &dstAddress)
+		if err != nil {
+			panic(err)
+		}
+
+		connection.Write([]byte(*message))
+
+		buffer := make([]byte, 1200)
+		_, err = connection.Read(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("received: %s\n", buffer)
+	}
 }
